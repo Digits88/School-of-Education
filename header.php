2666d2d0de69c4461eb0e5f18fb3cdbff67a5e10
Tuater Vue <tvue@cals.wisc.edu>
2015-05-20 14:38:08 -0500

    Modified CSS to get rid of iphone-centric responsiveness and implemented Mobile Menu to Merit Theme

--- header.php
+++ header.php
@@ -45,9 +45,10 @@
 
 	?></title>
 	
-<meta name="viewport" content="width=320.1, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" /> 
+<!-- <meta name="viewport" content="width=320.1, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" /> 
 <meta name="apple-mobile-web-app-capable" content="no">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-status-bar-style" content="black"> -->
+
  <!-- iOS Device Startup Images -->
  
 <!-- iPhone -->
@@ -102,8 +103,8 @@
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-css-transform.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-rotate.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/browserdetect.js"></script>
-   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mainactions.js"></script>
-
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mainactions.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/global.js"></script>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -131,10 +132,18 @@ $current_colorscheme = $options['link_color'];
 
 <body <?php body_class(); ?> id="<?php echo $current_colorscheme; ?>">
 
+
+<div class="mobileMenu">
+	<div class="searchBox"><?php get_search_form(); ?></div>
+	<div class="mobileScrollTop"></div>
+	<h2 class="mobileTitle">Menu</h2>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+</div>
+<div class="menuOverlay"></div>
 <div id="page" class="hfeed">
-	<header id="branding" role="banner">
-	
+	<header id="branding" role="banner">	
 			<hgroup class="heading">
+				<a href="#" class="mobileTrigger"><svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32; fill: #b7ab82;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg></a>	
 				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
 
@@ -217,91 +226,25 @@ wp_reset_query();
 
 			
 			</hgroup>
-			
 			<div class="utilityMenu">
 			<?php wp_nav_menu( array( 'theme_location' => 'utility' ) ); ?>
 			</div>
-			
-			<nav id="access" role="navigation">
-				<a href="#" class="mobileNavTrigger">Navigation</a>
+
+			<nav id="access" class ="main-navigation" role="navigation">
+				<div class="site-navigation-container">
+				<!-- <a href="#" class="mobileNavTrigger">Navigation</a>
 				<a href="#" class="mobileSettingsTrigger">Navigation</a>
-				<div class="mobileScrollTop"></div>
-				<div class="centerfix">
-				<a href="#" class="totop" title="Go to the top of the page">Go to the top of the page</a>
-				<h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
-				<?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
-				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
-				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to secondary content', 'twentyeleven' ); ?></a></div>
-				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
+				<div class="mobileScrollTop"></div> -->
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				
-				
-				<!-- Commented out to hardcode the site's navigation -->
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-				<!--<div class="menu">
-					<ul>
-						<li class="page_item">
-							<a href="/find/">Find</a>
-							<ul class="children">
-								<li class="page_item"><a href="/articles/">Articles</a></li>
-								<li class="page_item"><a href="/books/">Books</a></li>
-								<li class="page_item"><a href="/databases/">Databases</a></li>
-								<li class="page_item"><a href="/journals/">Journals</a></li>
-							</ul>
-						</li>
-						<li class="page_item">
-							<a href="/services/">Services</a>
-							<ul class="children">
-								<li class="page_item"><a href="/using-the-library/">Using the Library</a></li>
-							</ul>
-						</li>
-						<li class="page_item">
-							<a href="#">Research</a>
-							<ul class="children">
-								<li class="page_item"><a href="#">E-Books</a></li>
-							</ul>
-						</li>
-						<li class="page_item">
-							<a href="#">Libraries</a>
-							<ul class="children">
-								<li class="page_item"><a href="#">Steenbock</a></li>
-							</ul>
-						</li>
-						<li class="page_item">
-							<a href="#">About</a>
-							<ul class="children">
-								<li class="page_item"><a href="#">Hours</a></li>
-							</ul>
-						</li>
-						<li class="page_item">
-							<a href="#">Help</a>
-							<ul class="children">
-								<li class="page_item"><a href="#">Steenbock</a></li>
-							</ul>
-						</li>
-						<li class="page_item user_account">
-							<a href="#">Hi Jane Smith!</a>
-						</li>
-					</ul>
-				</div>-->
-				
-				
-				<!-- The markup of the navigation if it is hard coded -->
-				<!--<ul class="clearfix">
-			  		<li><a href="#">Students</a></li>
-			  		<li><a href="#">Alumni</a></li>
-			  		<li><a href="#">Faculty &amp; Staff</a></li>
-			  		<li><a href="#">Industry &amp; Community</a></li>
-			  		<li><a href="#">Research</a></li>
-			  		<li><a href="#">Outreach</a></li>
-  				</ul>-->
-				</div>
-				
+			</div>	
 			</nav><!-- #access -->
 			
 			<div class="navcornerleft"></div>
 			<div class="navcornerright"></div>
 <div class="headingbg clearfix"></div>
 	</header><!-- #branding -->
+<!--<div class="searchBox"><?php get_search_form(); ?></div>-->
 
 <div id="mobile-menu">
 	<div id="mobile-menu-inner">
---
+++ js/global.js
@@ -0,0 +1,185 @@
+jQuery (document ).ready(function($){
+
+
+/**
+ * ----------------------------------------------------------------------------
+ *
+ *  Mobile Menu Logic
+ *
+ * ----------------------------------------------------------------------------
+ */
+ 
+
+ var menuWidth = $(".site-navigation-container .menu").width();
+ var totalWidth = 0;
+ $(".site-navigation-container .menu > ul > li").each(function() {
+		totalWidth = totalWidth  + $(this).width();
+ });
+
+
+ function mobileMenu() {
+	  var windowWidth = $(window).width();
+	  var menuWidth = $(".site-navigation-container .menu").width();
+	   /*$(".menu > ul > li").each(function() {
+			totalWidth = totalWidth  + $(this).width();
+ 		});*/
+	 
+	 //console.log(totalWidth + " " + menuWidth);
+
+	 if(windowWidth < 600) {		
+		$(".navcornerleft").css("visibility","hidden");
+		$(".navcornerright").css("visibility","hidden");
+		$(".collegeFeature, .collegeFeature2").css("width","100%").css("margin-top","5em");
+		$("#main").css("margin-top", "4.58em");
+		//$("#page").css("margin-top", "4.65em");
+		$("footer .inner").css("width","100%");
+		$(".mobileTrigger").show();
+		$(".logoImage").addClass("mobileMenuOn");
+		$("#access").css("visibility","hidden").css("position", "absolute");
+		subMobileMenu();
+	 } else {
+	 
+		 if(totalWidth > menuWidth) {
+			 //console.log("switch to mobile");
+			 $(".navcornerleft").css("visibility","hidden");
+			 $(".navcornerright").css("visibility","hidden");
+			 $(".collegeFeature, .collegeFeature2").css("width","100%").css("margin-top","5em");
+			 $("#main").css("margin-top", "4.58em");
+			 //$("#page").css("margin-top", "4.65em");
+			 $("footer .inner").css("width","100%");
+			 $(".mobileTrigger").show();
+			 $(".logoImage").addClass("mobileMenuOn");
+			 $("#access").css("visibility","hidden").css("position", "absolute");
+			 subMobileMenu();
+		 } else {
+			 if(totalWidth == 0) {
+				 //console.log("switch to mobile");
+				 $(".navcornerleft").css("visibility","hidden");
+				 $(".navcornerright").css("visibility","hidden");
+				 $(".collegeFeature, .collegeFeature2").css("width","80%").css("margin-top","5em");
+				 $("#page").css("margin-top", "4.65em");
+				 $("footer .inner").css("width","100%");
+				 $("#main").css("margin-top", "4.58em");
+				 $(".mobileTrigger").show();
+				 $(".logoImage").addClass("mobileMenuOn");
+				 $("#access").css("visibility","hidden").css("position", "absolute");
+				 subMobileMenu();
+			 } else {
+				 //console.log("switch to full");		
+				 $(".navcornerleft").css("visibility","visible");
+				 $(".navcornerright").css("visibility","visible");
+				 $(".collegeFeature, .collegeFeature2").css("width","92.4%");
+				 $("footer .inner").css("width","95%");
+				 $(".blog .inner").css("width","97.6%");
+				 //$("#page").css("margin-top", "0");
+				 $(".collegeFeature2").css("margin-top","0");
+				 $("#main").css("margin-top", "0");
+				 $(".mobileTrigger").hide();
+				 $(".logoImage").removeClass("mobileMenuOn");
+				 $("#access").css("visibility","visible").css("position", "relative");	
+
+			 }
+		 }
+	 
+	 }
+	
+}
+
+mobileMenu();
+
+$(window).resize(function() {
+	mobileMenu();
+});
+
+$(".mobileTrigger, .menuOverlay").click(function(e) {
+	e.preventDefault();
+	
+	$(".mobileMenu").toggleClass("mobileMenuShown");
+	$(".menuOverlay").toggleClass("mobileMenuShown");
+	$("#page").toggleClass("blur");
+}); 
+
+	
+/**
+ * ----------------------------------------------------------------------------
+ *
+ *  Assign header menu controls
+ *
+ * ----------------------------------------------------------------------------
+ */
+ 
+	$(".youruwTrigger").click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		
+		$(".youruwmenu").toggleClass("visible");
+		
+	});
+	
+	$(".searchTrigger, .searchClose, .searchResultsOverlay").click(function(e) {
+		e.preventDefault();
+		
+		$(".searchUI").toggleClass("visible").attr("aria-hidden","false");
+		$("#page").toggleClass("blur");
+		$(".searchResultsOverlay").toggleClass("visible").attr("aria-hidden","false");
+		
+	});
+
+ 
+
+/**
+ * ----------------------------------------------------------------------------
+ *
+ *  SubMenu script
+ *
+ * ----------------------------------------------------------------------------
+ */
+function subMobileMenu(){
+
+	$('.menu-item a').click(function(event){
+		
+		//Create variable for current element
+		var elem = $(this);
+
+		//Check if the parent container of div has children links
+		if ($(elem).parent().hasClass('menu-item-has-children')){
+		event.preventDefault();
+		
+		//Grab submenu content and links
+		var subMenu = $(elem).next().html();
+		var elemText = $(elem).text() + " Overview";
+		var elemHref = $(elem).attr("href");
+
+		//Creating a label for overview link and back link
+		var overviewLink = "<li class=menu-item><a href='"+elemHref+"'>"+elemText+"</a></li>";
+		var backLink = "<li class=menu-item><a href='#' class='backLevel1'>Back</a></li>";
+
+		//Adding sub menu divs ontop of mobile menu div
+		$(".mobileMenu").append("<div class='subLevel1'></div>");
+		$(".subLevel1").append(subMenu).prepend(overviewLink).prepend(backLink);
+
+		//Delays the adding in sublevel for css transitions
+		setTimeout(function() {
+			//adds visible class
+			$(".subLevel1").addClass("visible");	
+			},100);
+		
+
+		//implement back link
+		$(".backLevel1").click(function(b) {
+			b.preventDefault();
+
+			$(".subLevel1").removeClass("visible");
+			//Delays the removal for sublevel for css transitions
+			setTimeout(function() {
+				$(".subLevel1").remove();
+			},300);
+
+		});
+ 	}
+	});
+
+ }
+//subMobileMenu();
+
+});
